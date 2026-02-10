a3db342167edee3f1a52afa20458b65ad3c15ae8
Aryhnr <ytminecraft93@gmail.com>
2026-02-10 08:09:29 +0700

    memperbaiki code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -8,12 +7,11 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-[#f8f8f8] selection:bg-black selection:text-white font-sans antialiased text-zinc-900">
-        <Navbar />
         <main className="grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/project/:slug" element={<ProjectDetail />} />
-            </Routes>
+          </Routes>
         </main>
         <Footer />
       </div>
